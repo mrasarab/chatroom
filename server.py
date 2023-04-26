f86aa7969611f9bb3ef83c66fa7e77fc65bb7ceb
mrasarab <120579485+mrasarab@users.noreply.github.com>
2023-04-27 00:46:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,73 +3,73 @@ import socket
 import threading
 
 # Set the host and port for the server
-host = '127.0.0.1'
-port = 5678
+host_server = '127.0.0.1'
+port_server = 5678
 
 # Create a new server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the server socket to the host and port
-server.bind((host, port))
+server.bind((host_server, port_server))
 
 # Listen for incoming connections
 server.listen()
 
-# Initialize lists to store connected clients and their aliases
-clients = []
-aliases = []
+# Initialize lists to store connected users and their all_user_nicknames
+users = []
+all_user_nicknames = []
 
-# Function to broadcast a message to all connected clients
+# Function to broadcast a message to all connected users
 def broadcast(message):
-    for client in clients:
-        client.send(message)
+    for user in users:
+        user.send(message)
 
-# Function to handle a single client connection
-def handle_client(client):
+# Function to handle a single user connection
+def handle_user(user):
     while True:
         try:
-            # Receive a message from the client
-            message = client.recv(1024)
-            # Broadcast the message to all clients
+            # Receive a message from the user
+            message = user.recv(1024)
+            # Broadcast the message to all users
             broadcast(message)
         except:
-            # If an error occurs, remove the client from the list of connected clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            # Get the client's alias and remove it from the list of aliases
-            alias = aliases[index]
-            aliases.remove(alias)
-            # Broadcast a message indicating that the client has left the chat room
-            broadcast(f'{alias} has left the chatroom'.encode('utf-8'))
+            # If an error occurs, remove the user from the list of connected users
+            index = users.index(user)
+            users.remove(user)
+            user.close()
+            # Get the user's nick_name and remove it from the list of all_user_nicknames
+            nick_name = all_user_nicknames[index]
+            all_user_nicknames.remove(nick_name)
+            # Broadcast a message indicating that the user has left the chat room
+            broadcast(f'{nick_name} has left the chatroom'.encode('utf-8'))
             break
 
-# Main function to handle incoming client connections
+# Main function to handle incoming user connections
 def receive():
     while True:
         # Print a message indicating that the server is running and listening for connections
         print('server is running and listening ...')
-        # Accept a new client connection
-        client, address = server.accept()
+        # Accept a new user connection
+        user, address = server.accept()
         # Print a message indicating that a new connection has been established
         print(f'connection is established with {str(address)}')
-        # Ask the client to choose an alias
-        client.send('alias?'.encode('utf-8'))
-        # Receive the alias from the client
-        alias = client.recv(1024)
-        # Add the client and their alias to the appropriate lists
-        aliases.append(alias)
-        clients.append(client)
-        # Print a message indicating the client's alias
-        print(f'the alias of this client is {alias}'.encode('utf-8'))
-        # Broadcast a message indicating that the client has joined the chat room
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        # Send a welcome message to the client
-        client.send('you are now connected!'.encode('utf-8'))
-        # Create a new thread to handle the client's messages
-        thread = threading.Thread(target=handle_client, args=(client,))
+        # Ask the user to choose an nick_name
+        user.send('nick_name?'.encode('utf-8'))
+        # Receive the nick_name from the user
+        nick_name = user.recv(1024)
+        # Add the user and their nick_name to the appropriate lists
+        all_user_nicknames.append(nick_name)
+        users.append(user)
+        # Print a message indicating the user's nick_name
+        print(f'the nick_name of this user is {nick_name}'.encode('utf-8'))
+        # Broadcast a message indicating that the user has joined the chat room
+        broadcast(f'{nick_name} has connected to the chat room'.encode('utf-8'))
+        # Send a welcome message to the user
+        user.send('you are now connected!'.encode('utf-8'))
+        # Create a new thread to handle the user's messages
+        thread = threading.Thread(target=handle_user, args=(user,))
         thread.start()
 
-# Start the main function to listen for incoming client connections
+# Start the main function to listen for incoming user connections
 if __name__ == "__main__":
     receive()
